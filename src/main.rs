d23a5aa1e8e450baab4228937a805c4eefe9df9e
Matt Valentine-House <matt@eightbitraptor.com>
2015-07-29 21:14:05 +0100

    Create WAV of a Sine wave using Hound
    Hound is a library that abstracts creating and reading WAV files with
    Rust. This commit adds Hound to the project and introduces a function
    `write_sine_wav` which uses the mathematical formula A.sin(2π𝒇𝒕) to
    produce a sine wave.
    
    Because we're using a computer and therefore must be using discrete
    sampling we choose a sampling frequency (44.1kHz) and an appropriate
    number of bits per sample (16), and we use the formula above to generate
    88200 samples and write them to the wav file.
    
    88200 samples at 44.1kHz (44100 samples per second) produces enough
    points to build a 2 second long audio clip.
    
    I picked 44.1kHz and 16 bits purely because they are the values used by
    the CD-DA format used on audio CD's and defined by Sony and Phillips in
    The Red Book in 1982[1]
    
    [1]: https://en.wikipedia.org/wiki/Compact_Disc_Digital_Audio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate hound;
+
+use std::f32::consts::PI;
+use std::i16;
+use hound::*;
+
+fn write_sine_wav() {
+    let sampling_freq = 44100;           // 44.1 kHz, a common sampling frequency
+    let sampling_bits = 16;              // 16 bits per sample, a la CDDA
+    let amplitude     = i16::MAX as f32; // Make it loud
+    let note_freq     = 440.0;           // A440, the A abouve middle C.
+    let length        = 2;               // length of the sound in seconds
+
+    let no_of_samples  = sampling_freq * length;
+    let normalized_sample_indices = (0 .. no_of_samples).
+        map(|x| x as f32 / sampling_freq as f32);
+
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: sampling_freq,
+        bits_per_sample: sampling_bits
+    };
+
+    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
+    for t in normalized_sample_indices {
+        let sample = (t * note_freq * 2.0 * PI).sin();
+        writer.write_sample((sample * amplitude) as i16).unwrap();
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    write_sine_wav();
 }
